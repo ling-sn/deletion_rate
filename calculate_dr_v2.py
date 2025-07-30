3ca1f570901682c4b737965dc1a8340c42ce63ba
Sonia <lingsn@umich.edu>
2025-07-30 13:30:45 -0400

    drop negative values in RealRate

--- calculate_dr_v2.py
+++ calculate_dr_v2.py
@@ -106,6 +106,7 @@ def open_bam(folder_name):
                     denom = (df_final["fit_c"]*(df_final["fit_b"] + df_final["fit_s"] -
                              df_final["fit_s"]*df_final["DeletionRate"]-1))
                     df_final["RealRate"] = num/denom
+                    df_final = df_final[df_final["RealRate"] >= 0]
 
                     ## add all calculations to og dataframe & save as .tsv output
                     df_final.to_csv(output_tsv_name, sep = "\t", index = False)
