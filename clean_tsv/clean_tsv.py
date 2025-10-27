dd17a37a71b431934c6a082fee362a636c96a0bd
Sonia <lingsn@umich.edu>
2025-10-27 14:55:14 -0400

    change criterion to filter average deletions/deletionrate across all replicates

--- clean_tsv/clean_tsv.py
+++ clean_tsv/clean_tsv.py
@@ -111,8 +111,8 @@ class FilterTSV:
          1. Pvalue across all replicates < 0.0001
          2. RealRate across all replicates > 0.3
          3. Total sequencing coverage for each BS and NBS replicate > 20
-         4. Average Deletions for each BS replicate > 5
-         5. Average DeletionRate for each BS replicate > 0.02
+         4. Average Deletions across all BS replicates > 5
+         5. Average DeletionRate across all BS replicates > 0.02
          6. Average DeletionRate is 2x higher in BS replicate compared to NBS replicate
       b) Saves filtered and discarded rows in separate dataframes
       """
