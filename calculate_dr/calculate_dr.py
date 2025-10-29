8246af9ec9baaee05f9237a9ee3f4035cfc11fc6
Sonia <lingsn@umich.edu>
2025-10-29 10:52:46 -0400

    make code more readability; fix line breaks

--- calculate_dr/calculate_dr.py
+++ calculate_dr/calculate_dr.py
@@ -10,33 +10,49 @@ import re
 
 def pysam_pileup(bamfile, chrom, mod_base, base_ct):
     """
-    Counts number of bases/deletions in PileupColumn 
-    for each GenomicModBase
+    PURPOSE:
+    Counts # of bases/deletions in PileupColumn for each GenomicModBase
+    ---
+    NOTES:
+    * Indicate the interval int(mod_base-1) TO int(mod_base) because:
+      1. pysam is 0-based, while GenomicModBase is 1-based
+      2. python uses half-open intervals, so this denotes a single coord
+    * pileupcolumn.set_min_base_quality(0): Prevents pysam from filtering
+      on base quality
+    * base = pileupread.alignment.query_sequence[pileupread.query_position]:
+      Taken from example in pysam manual; returns base letter
     """
     try:
-        for pileupcolumn in bamfile.pileup(str(chrom), int(mod_base-1), int(mod_base), truncate = True): ## pysam is 0-based, while GenomicModBase was 1-based
-            pileupcolumn.set_min_base_quality(0) ## prevent pysam from filtering based on base quality
+        for pileupcolumn in bamfile.pileup(str(chrom), 
+                                           int(mod_base-1), int(mod_base), 
+                                           truncate = True): 
+            pileupcolumn.set_min_base_quality(0)
             if pileupcolumn.pos == int(mod_base-1):
                 for pileupread in pileupcolumn.pileups:
                     if pileupread.is_del and not pileupread.is_refskip:
                         base_ct["Deletions"] += 1
                     elif not pileupread.is_refskip:
-                        base = pileupread.alignment.query_sequence[pileupread.query_position] ## taken from example in manual; returns base letter
+                        base = pileupread.alignment.query_sequence[pileupread.query_position]
                         if base in base_ct:
                             base_ct[base] += 1
                         else:
                             continue
     except Exception as e:
-        print(f"Failed to count bases/deletions in PileupColumn for Chromosome {chrom} at GenomicModBase {mod_base}: {e}")
+        print("Failed to count bases/deletions in PileupColumn for"
+              f"Chromosome {chrom} at GenomicModBase {mod_base}: {e}")
         traceback.print_exc()
         raise
 
 def count_base(chunk, input_bam_name, results):
     bamfile = pysam.AlignmentFile(input_bam_name, "rb")
     """
+    PURPOSE:
     Counts number of bases/deletions for each UNUAR site
-        1) chrom: value in "Chrom" column (e.g., NW_018654708.1)
-        2) mod_base: value in "GenomicModBase" column (e.g., 373)
+    ---
+    NOTES:
+    * chrom: value in "Chrom" column (e.g., NW_018654708.1)
+    * mod_base: value in "GenomicModBase" column (e.g., 373)
+    * **base_ct: Unpacks base_ct dict
     """
     try:
         for row in chunk:
@@ -48,7 +64,7 @@ def count_base(chunk, input_bam_name, results):
 
             results.append({"Chrom": chrom,
                             "GenomicModBase": mod_base,
-                            **base_ct}) ## unpack base_ct dict in this dict
+                            **base_ct})
         bamfile.close()
     except Exception as e:
         print(f"Failed to count bases/deletions in UNUAR sites: {e}")
@@ -59,7 +75,8 @@ def process_chunk(genome_coord, input_bam_name, results):
     try:
         all_chunks = np.array_split(genome_coord, 100)
         with concurrent.futures.ThreadPoolExecutor(max_workers = 12) as executor:
-            futures = [executor.submit(count_base, chunk, input_bam_name, results) for chunk in all_chunks]
+            futures = [executor.submit(count_base, chunk, input_bam_name, results) 
+                       for chunk in all_chunks]
             for future in concurrent.futures.as_completed(futures):
                 future.result()
     except Exception as e:
@@ -69,20 +86,23 @@ def process_chunk(genome_coord, input_bam_name, results):
 
 def make_key(subfolder, base_key):
     """
-    Modifies names of dictionary keys based on 
-    the Replicate # (detected via RegEx) and Sample Type (BS, NBS)
-    in a given subfolder name.
+    PURPOSE:
+    Modifies names of dictionary keys based on the Rep # (detected via RegEx)
+    and Sample Type (BS, NBS) in a given subfolder name.
+    ---
+    NOTES:
+    * sorted(set(rep_matches)): Removes duplicate reps, sorts in ascending order
+    * for rep in rep_list: Adds replicate prefix to dict key names
+    * for sample in ['BS', 'NBS']: Adds sample type suffix to dict key names
     """
     rep_matches = re.findall(r"Rep\d+", str(subfolder))
-    rep_list = sorted(set(rep_matches)) ## removes duplicate reps and sorts in ascending order
+    rep_list = sorted(set(rep_matches), key = lambda x: int(x[3:]))
 
-    ## Adds replicate prefix to dictionary key names
     for rep in rep_list:
         if f"-{rep}-" in str(subfolder):
             prefix = rep + "_"
             break
     
-    ## Adds sample type suffix to dictionary key names
     for sample in ["BS", "NBS"]:
         if f"-{sample}_" in str(subfolder):
             suffix = "_" + sample
@@ -92,8 +112,12 @@ def make_key(subfolder, base_key):
 
 def match_regex(folder_name):
     """
-    Given input folder names, extract the group name.
-        EXAMPLE: '7KO-Cyto-BS_processed_fastqs' -> '7KO-Cyto'
+    PURPOSE:
+    Given input folder names, extract the group name
+    by returning the first capture group in RegEx.
+    ---
+    EXAMPLE: 
+    '7KO-Cyto-BS_processed_fastqs' -> '7KO-Cyto'
     """
     try:
         match = re.match(r"(.+)-(?:BS|NBS)_processed_fastqs", folder_name)
@@ -101,23 +125,24 @@ def match_regex(folder_name):
         print(f"Failed to RegEx match input folder to group: {e}")
         traceback.print_exc()
         raise
-    return match.group(1) ## return first capture group
+    return match.group(1)
 
-## main code
 def main(folder_name):
     """
-    Opens .bam in folder
-    and runs calculations
+    PURPOSE: 
+    Opens .bam in folder and runs calculations
     """
     current_path = Path.cwd()
     input_dir = current_path/"realignments"/folder_name
     group_name = match_regex(folder_name)
     
-    left = pd.read_csv(Path("~/umms-RNAlabDATA/Software/B-PsiD_tools/UNUAR_motif_sites_mRNA_hg38p14.tsv").expanduser(), sep = "\t")
-    right = pd.read_excel(Path("~/umms-RNAlabDATA/Software/B-PsiD_tools/Zhang_HE_NatureProtocols_2023_SupplementaryTable1.xlsx").expanduser())
+    left = pd.read_csv(Path("~/umms-RNAlabDATA/Software/B-PsiD_tools"
+                            "/UNUAR_motif_sites_mRNA_hg38p14.tsv").expanduser(), sep = "\t")
+    right = pd.read_excel(Path("~/umms-RNAlabDATA/Software/B-PsiD_tools"
+                               "/Zhang_HE_NatureProtocols_2023_SupplementaryTable1.xlsx").expanduser())
 
     df = pd.merge(left, right, how = "left", on = "Motif")
-    genome_coord = df[["Chrom", "GenomicModBase"]].to_numpy() ## faster processing
+    genome_coord = df[["Chrom", "GenomicModBase"]].to_numpy()
     
     try: 
         for subfolder in input_dir.iterdir():
@@ -125,45 +150,51 @@ def main(folder_name):
                 processed_folder = current_path/"calculations"/group_name/"individual_tsv"
                 processed_folder.mkdir(exist_ok=True, parents=True)
                 
-                key = {base_key: make_key(subfolder, base_key) for base_key in ["A", "C", "G", "T", "Deletions", "DeletionRate", "RealRate"]}
+                key = {base_key: make_key(subfolder, base_key) for base_key 
+                       in ["A", "C", "G", "T", "Deletions", "DeletionRate", "RealRate"]}
                 
                 for bam in subfolder.glob("*.bam"):
                     results = []
-                    input_bam_name = Path(bam) ## turn string from list back into filepath
+
+                    ## Turn string from list back into filepath
+                    input_bam_name = Path(bam)
                     output_tsv_name = processed_folder/f"{input_bam_name.stem}.tsv"
                     
-                    ## count A, C, G, T and deletions @ each UNUAR site
+                    ## Count A, C, G, T and deletions @ each UNUAR site
                     process_chunk(genome_coord, input_bam_name, results)
 
-                    ## calculate observed deletion rates
+                    ## Calculate observed deletion rates
                     counts = pd.DataFrame(results)
-                    total_sum = counts[["A", "C", "G", "T", "Deletions"]].sum(axis = 1) ## sum across rows
+                    total_sum = counts[["A", "C", "G", "T", "Deletions"]].sum(axis = 1)
                     counts["DeletionRate"] = counts["Deletions"]/total_sum
                     
-                    ## calculate real deletion rates
-                    df_draft = pd.merge(df, counts, how = "left", on = ["Chrom", "GenomicModBase"]).dropna()
+                    ## Calculate real deletion rates
+                    df_draft = pd.merge(df, counts, how = "left", 
+                                        on = ["Chrom", "GenomicModBase"]).dropna()
                     num = df_draft["fit_b"] - df_draft["DeletionRate"]
                     denom = (df_draft["fit_c"] * (df_draft["fit_b"] + df_draft["fit_s"] -
                              df_draft["fit_s"] * df_draft["DeletionRate"] - 1))
                     df_draft["RealRate"] = num/denom
                     df_final = df_draft.rename(columns = {"A": key["A"], 
-                                                        "C": key["C"], 
-                                                        "G": key["G"], 
-                                                        "T": key["T"], 
-                                                        "Deletions": key["Deletions"], 
-                                                        "DeletionRate": key["DeletionRate"], 
-                                                        "RealRate": key["RealRate"]})
+                                                          "C": key["C"], 
+                                                          "G": key["G"], 
+                                                          "T": key["T"], 
+                                                          "Deletions": key["Deletions"], 
+                                                          "DeletionRate": key["DeletionRate"], 
+                                                          "RealRate": key["RealRate"]})
                     
-                    ## add all calculations to og dataframe & save as .tsv output
+                    ## Save as .tsv output
                     df_final.to_csv(output_tsv_name, sep = "\t", index = False)
 
     except Exception as e:
-        print(f"Failed to calculate observed & real deletion rates in {folder_name} and save as .tsv: {e}")
+        print("Failed to calculate observed & real deletion rates in "
+              f"{folder_name} and save as .tsv: {e}")
         traceback.print_exc()
         raise
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description = "Calculates observed and real deletion rates for every UNUAR site in a BAM file.")
+    parser = argparse.ArgumentParser(description = "Calculates observed and real deletion rates" 
+                                                   "for every UNUAR site in a BAM file.")
     parser.add_argument("--folder_name", help = "Name of processed_fastqs folder", required = True)
     args = parser.parse_args()
 
