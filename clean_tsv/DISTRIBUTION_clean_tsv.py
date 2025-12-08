78b0d8cae91537b75afd18e13b9bab64a727429e
Sonia <lingsn@umich.edu>
2025-12-08 16:27:49 -0500

    change text placement

--- clean_tsv/DISTRIBUTION_clean_tsv.py
+++ clean_tsv/DISTRIBUTION_clean_tsv.py
@@ -8,65 +8,79 @@ from itertools import chain
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def graph_plots(counter, graph_folder):
-   try: 
-      if counter == 1:
-         col = "TotalCoverage"
+class GraphPlots:
+   def total_cov(self, df, graph_folder, total_cov):
+      """
+      PURPOSE: Graphs distributions for TotalCoverage
+      """
 
-         ## Create histogram
-         hist_fig = plt.figure(figsize = (10, 6.5))
-         sns.displot(data = df, x = col, 
-                     kde = True, edgecolor = None, shrink = 0.90)
-         plt.title(f"Figure {counter}: Histogram of all {col}")
-         hist_fig.savefig(graph_folder/f"Fig{counter}_{col}_Histogram", format = "png", dpi = 300)
-         plt.close()
+      col = "TotalCoverage"
+      
+      ## Create histogram
+      hist_fig = plt.figure(figsize = (10, 6.5))
+      sns.displot(data = df, x = col, 
+                  kde = True, edgecolor = None)
+      plt.title(f"Figure {counter}: Histogram of all {col}")
+      plt.ylim(0, 125000)
+      plt.xlim(0, 200)
+      hist_fig.savefig(graph_folder/f"Fig{counter}_{col}_Histogram", format = "png", dpi = 300)
+      plt.close()
 
-         ## Create ECDF and plot median
-         ecdf_fig = plt.figure(figsize = (10, 6.5))
-         sns.ecdfplot(total_cov[col])
-         median = total_cov[col].median()
-         plt.axvline(x = median, color = "red", ls = ":", lw = 1.5, alpha = 0.3)
-         plt.axhline(y = 0.5, color = "red", ls = ":", lw = 1.5, alpha = 0.3)
-         plt.text(median, 0.52, f"Median: {median:<6}", 
-                  horizontalalignment = "right", 
-                  verticalalignment = "bottom") 
-         counter += 1
-         plt.title(f"Figure {counter}: ECDF of all {col}")
-         ecdf_fig.savefig(graph_folder/f"Fig{counter}_{col}_ECDF", format = "png", dpi = 300)
-         plt.close()
-      else:
-         col = "DeletionRate"
-         key = str(next(key for key, val in df_name.items() if val.equals(df)))
-         sample_group = "-".join(key.split("_")[0:2]).upper()
+      ## Create ECDF and plot median
+      ecdf_fig = plt.figure(figsize = (10, 6.5))
+      sns.ecdfplot(total_cov[col])
+      median = total_cov[col].median()
+      plt.axvline(x = median, color = "red", ls = ":", lw = 1.5, alpha = 0.3)
+      plt.axhline(y = 0.5, color = "red", ls = ":", lw = 1.5, alpha = 0.3)
+      plt.text(500, 0.01, s = f"Median: {median:<6}",
+               horizontalalignment = "right", 
+               verticalalignment = "bottom") 
+      counter += 1
+      plt.title(f"Figure {counter}: ECDF of all {col}")
+      plt.xlim(0, 500)
+      ecdf_fig.savefig(graph_folder/f"Fig{counter}_{col}_ECDF", format = "png", dpi = 300)
+      plt.close()
 
-         hist_fig = plt.figure(figsize = (10, 6.5))
-         sns.displot(data = df, x = col, 
-                     kde = True, edgecolor = None, shrink = 0.90)
-         counter += 1
-         plt.title(f"Figure {counter}: Histogram of all {col} in {sample_group}")
-         hist_fig.savefig(graph_folder/f"Fig{counter}_{sample_group}_{col}_Histogram", 
-                        format = "png", dpi = 300)
-         plt.close()
+      return counter
 
-         ## Create ECDF and plot median
-         ecdf_fig = plt.figure(figsize = (10, 6.5))
-         sns.ecdfplot(df[col])
-         median = df[col].median()
-         plt.axvline(x = median, color = "red", ls = ":", lw = 1.5, alpha = 0.3)
-         plt.axhline(y = 0.5, color = "red", ls = ":", lw = 1.5, alpha = 0.3)
-         plt.text(median, 0.52, f"Median: {median:<6}", 
-                  horizontalalignment = "right", 
-                  verticalalignment = "bottom") 
-         counter += 1
-         plt.title(f"Figure {counter}: ECDF of all {col} in {sample_group}")
-         ecdf_fig.savefig(graph_folder/f"Fig{counter}_{sample_group}_{col}_ECDF", 
-                        format = "png", dpi = 300)
-         plt.close()
-   except Exception as e:
-      print(f"Failed to create distribution graphs: {e}")
-      traceback.print_exc()
-      raise
-   return
+   def graph_all(self, counter, df, df_name, graph_folder):
+      try: 
+         if counter == 1:
+            col = "TotalCoverage"
+
+
+         else:
+            col = "DeletionRate"
+            key = str(next(key for key, val in df_name.items() if val.equals(df)))
+            sample_group = "-".join(key.split("_")[0:2]).upper()
+
+            hist_fig = plt.figure(figsize = (10, 6.5))
+            sns.displot(data = df, x = col, 
+                        kde = True, edgecolor = None, shrink = 0.90)
+            counter += 1
+            plt.title(f"Figure {counter}: Histogram of all {col} in {sample_group}")
+            hist_fig.savefig(graph_folder/f"Fig{counter}_{sample_group}_{col}_Histogram", 
+                           format = "png", dpi = 300)
+            plt.close()
+
+            ## Create ECDF and plot median
+            ecdf_fig = plt.figure(figsize = (10, 6.5))
+            sns.ecdfplot(df[col])
+            median = df[col].median()
+            plt.axvline(x = median, color = "red", ls = ":", lw = 1.5, alpha = 0.3)
+            plt.axhline(y = 0.5, color = "red", ls = ":", lw = 1.5, alpha = 0.3)
+            plt.text(median, 0.52, f"Median: {median:<6}", 
+                     horizontalalignment = "right", 
+                     verticalalignment = "bottom") 
+            counter += 1
+            plt.title(f"Figure {counter}: ECDF of all {col} in {sample_group}")
+            ecdf_fig.savefig(graph_folder/f"Fig{counter}_{sample_group}_{col}_ECDF", 
+                           format = "png", dpi = 300)
+            plt.close()
+      except Exception as e:
+         print(f"Failed to create distribution graphs: {e}")
+         traceback.print_exc()
+         raise
 
 def concat_reps(suffix, tsv_list, subfolder, processed_folder):
    """
@@ -135,6 +149,9 @@ def main():
    current_path = Path.cwd()
    input_dir = current_path/"calculations"
 
+   ## Initialize class
+   graph = GraphPlots()
+
    try: 
       processed_folder = current_path/"merged"
       processed_folder.mkdir(exist_ok = True, parents = True)
@@ -189,8 +206,12 @@ def main():
       
       ## Run graph_plots function
       sns.set_palette(palette = "plasma_r")
+      plt.rcParams["font.family"] = "Cambria"
+
       for df in df_graphs:
          # PLACEHOLDER
+         print(df)
+         # use graphs class member
 
    except Exception as e:
       print(f"Failed to create merged .tsv files: {e}")
